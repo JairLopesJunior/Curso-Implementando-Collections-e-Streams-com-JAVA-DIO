d00223b5fb8ef6ae685b6cb7b9838561bba51ca3
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-14 19:23:07 -0300

    Finalizado o conhecimento com a Collections Queue

---
+++ br.com.digitalinovationone.collectionsestreamscomjava/src/br/com/digitalinovationone/collectionsestreamscomjava/list/ExercicioQueue.java
@@ -0,0 +1,60 @@
+package br.com.digitalinovationone.collectionsestreamscomjava.list;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ExercicioQueue {
+	
+	// Crie uma fila e execute as seguintes operações:
+	// Adicione 5 nomes: Juliana, Pedro, Carlos, Larissa e João.
+	// Navegue na fila exibindo cada nome no console.
+	// Retorne o primeiro item da fila, sem remove-lo.
+	// Retorne o primeiro item da fila, removendo o mesmo.
+	// Adicione um novo nome: Daniel. Verifique a posição que o mesmo assumiu na fila.
+	// Retorne o tamanho da fila.
+	// Verifique se a fila esta vazia.
+	// Verifique se o nome Carlos esta na fila.
+
+	public static void main(String[] args) {
+		
+		Queue<String> fila = new LinkedList<>();
+		
+		fila.add("Juliana");
+		fila.add("Pedro");
+		fila.add("Carlos");
+		fila.add("Larissa");
+		fila.add("João");
+		
+		System.out.println(fila); // Mostrando a fila
+		
+		fila.forEach(f -> {
+			System.out.println(f);
+		});
+		
+		String nomeRetornado = fila.peek(); // Retornando primeiro elemento sem remove-lo
+		
+		System.out.println(nomeRetornado + " " + fila);
+		
+		String elementoRemovido = fila.poll(); // Retornando elemento removido
+		
+		System.out.println(elementoRemovido);
+		System.out.println(fila);
+		
+		fila.add("Daniel"); // Adicionando novo Elemento
+		
+		System.out.println(fila);
+		
+		int tamanho = fila.size(); // Retorna o tamanho da fila
+		
+		System.out.println(tamanho);
+		
+		boolean f = fila.isEmpty();
+		
+		System.out.println(f);
+		
+		boolean tem = fila.contains("Carlos");
+		
+		System.out.println(tem);
+	}
+
+}
---
+++ br.com.digitalinovationone.collectionsestreamscomjava/src/br/com/digitalinovationone/collectionsestreamscomjava/list/LinkedListJava.java
@@ -0,0 +1,52 @@
+package br.com.digitalinovationone.collectionsestreamscomjava.list;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinkedListJava {
+
+	public static void main(String[] args) {
+		
+		Queue<String> fila = new LinkedList<>();
+		
+		fila.add("Patricia");
+		fila.add("Roberto");
+		fila.add("Flavio");
+		fila.add("Pamela");
+		fila.add("Anderson");
+		
+		System.out.println(fila);
+		String clienteASerAtendido = fila.poll(); // Ele remove o primeiro elemento e retorna o elemento removido
+		
+		System.out.println(clienteASerAtendido);
+		System.out.println(fila);
+		
+		String primeiroCliente = fila.peek(); // Ele retorna o primeiro elemento da fila, mas não remove, caso a fila esteja vazia retorna null
+		
+		System.out.println(primeiroCliente);
+		System.out.println(fila);
+		
+		//fila.clear();
+		
+		String primeiroElementoOuErro = fila.element(); // Ele retorna o primeiro elemento da fila, caso esteja vazia ele da uma exception
+		
+		System.out.println(primeiroElementoOuErro);
+		System.out.println(fila);
+
+		for(String f : fila) {
+			System.out.println(f);
+		}
+		
+		Iterator<String> iteratorFila = fila.iterator();
+		
+		while(iteratorFila.hasNext()) {
+			System.out.println("-" + iteratorFila.next());
+		}
+		
+		System.out.println(fila.size()); // Retorna o tamanho da Lista, contando os elementos
+		//fila.clear();
+		System.out.println(fila.isEmpty()); // Retorna true se a lista ter vazia ou false caso contrario
+	}
+	
+}
